fd374cad96f3951fe9ab0ecc1f3d0734e92f5f13
Nuel Elekwachi <elekwaeke300@gmail.com>
2023-04-25 01:05:36 +0100

    clean up

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-// const links = document.querySelector(".links");
-// const hamburger = document.querySelector(".hamburger");
-
-// hamburger.addEventListener("click", () => {
-//   links.classList.toggle("active");
-//   //   console.log("hi");
-// });
-
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".links");
 
@@ -14,5 +6,4 @@ hamburger.addEventListener("click", mobileMenu);
 function mobileMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
-  console.log("first");
 }
